7dcae03145c6eb80236288738afbba0416ceabe2
Elnour Tech <Elnour Tech@DESKTOP-C6QMATS>
2023-09-27 14:30:03 +0300

    Add project files.

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+            //modelBuilder.Entity<PortfolioItem>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+
+            //modelBuilder.Entity<Owner>().HasData(
+            //    new Owner()
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Avatar = "avatar.jpg",
+            //        FullName = "Khalid ESSAADANI",
+            //        Profil = "Microsoft MVP / .NET Consultant"
+            //    }
+            //    );
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<PortfolioItem> PortfolioItems { get; set; }
+    }
+}
---
+++ Web/ViewModels/PortfolioViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Web.ViewModels
+{
+    public class PortfolioViewModel
+    {
+        public int Id { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
